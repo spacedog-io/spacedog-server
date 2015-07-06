e50109d64041a6cbaf491c17a27a8aa3682bfbf6
David Attias <attias666@gmail.com>
2015-07-06 14:49:44 +0200

    Style changes

--- src/main/java/com/magiclabs/restapi/SchemaResource.java
+++ src/main/java/com/magiclabs/restapi/SchemaResource.java
@@ -100,7 +100,7 @@ public class SchemaResource extends AbstractResource {
 			return created("schema", type);
 
 		} catch (Throwable throwable) {
-			return AbstractResource.toPayload(throwable);
+			return toPayload(throwable);
 		}
 	}
 
