0c458f90b23a894b6284f5efc27f251f9d6bf1aa
David Attias <attias666@gmail.com>
2016-01-13 17:09:03 +0100

    Target should be defined by system properties

--- examples/src/main/java/io/spacedog/examples/SuperDogStatus.java
+++ examples/src/main/java/io/spacedog/examples/SuperDogStatus.java
@@ -6,12 +6,10 @@ package io.spacedog.examples;
 import org.junit.Assert;
 
 import io.spacedog.client.SpaceRequest;
-import io.spacedog.client.SpaceTarget;
 
 public class SuperDogStatus extends Assert {
 
 	public static void main(String[] args) throws Exception {
-		SpaceRequest.setTarget(SpaceTarget.production);
 		SpaceRequest.get("/v1/admin/account").superdogAuth().go(200);
 	}
 
