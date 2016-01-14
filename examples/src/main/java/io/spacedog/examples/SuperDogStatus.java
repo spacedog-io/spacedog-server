63130eff92846fdbae2f0ad37fc05a31e67f7389
David Attias <attias666@gmail.com>
2016-01-14 12:21:54 +0100

    SuperDogStatus shows account logs

--- examples/src/main/java/io/spacedog/examples/SuperDogStatus.java
+++ examples/src/main/java/io/spacedog/examples/SuperDogStatus.java
@@ -3,14 +3,28 @@
  */
 package io.spacedog.examples;
 
+import java.util.Iterator;
+
 import org.junit.Assert;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import io.spacedog.client.SpaceRequest;
 
 public class SuperDogStatus extends Assert {
 
 	public static void main(String[] args) throws Exception {
-		SpaceRequest.get("/v1/admin/account").superdogAuth().go(200);
+		ObjectNode accounts = SpaceRequest.get("/v1/admin/account").queryString("size", "100")//
+				.superdogAuth().go(200).objectNode();
+
+		Iterator<JsonNode> elements = accounts.get("results").elements();
+		while (elements.hasNext()) {
+			String backendId = elements.next().get("backendId").asText();
+			SpaceRequest.get("/v1/admin/log/" + backendId)//
+					.superdogAuth()//
+					.go(200);
+		}
 	}
 
 }
