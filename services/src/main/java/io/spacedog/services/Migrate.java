90fde1b14df1782e2368509097ca58f88391f5bd
David Attias <attias666@gmail.com>
2016-03-02 12:24:23 +0100

    Elastic data migration program second draft

--- services/src/main/java/io/spacedog/services/Migrate.java
+++ services/src/main/java/io/spacedog/services/Migrate.java
@@ -8,16 +8,26 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.concurrent.ExecutionException;
 
+import org.elasticsearch.action.bulk.BulkItemResponse;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.cluster.health.ClusterHealthStatus;
 import org.elasticsearch.cluster.metadata.MappingMetaData;
 import org.elasticsearch.common.collect.ImmutableOpenMap;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.settings.Settings.Builder;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.indices.IndexAlreadyExistsException;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.plugin.deletebyquery.DeleteByQueryPlugin;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import io.spacedog.utils.Json;
 
 public class Migrate {
 
@@ -36,8 +46,12 @@ public class Migrate {
 				"/Users/davattias/dev/spacedog/local/spacedog.server.properties");
 
 		Migrate singleton = new Migrate();
+
 		try {
 			singleton.startLocalElastic();
+			singleton.waitForClusterInit();
+			singleton.removeReplicas();
+			singleton.waitForClusterInit();
 			singleton.migrate();
 
 		} catch (Throwable t) {
@@ -48,14 +62,14 @@ public class Migrate {
 				if (singleton.node != null)
 					singleton.elastic.close();
 			}
-			System.exit(-1);
 		}
+
 	}
 
 	private void startLocalElastic() throws InterruptedException, ExecutionException, IOException {
 
 		Builder builder = Settings.builder()//
-				.put("node.local", true)//
+				.put("node.master", true)//
 				.put("node.data", true)//
 				.put("cluster.name", "spacedog-elastic-cluster")//
 				.put("path.home", //
@@ -75,38 +89,106 @@ public class Migrate {
 		elastic = new ElasticClient(node.client());
 	}
 
+	private void waitForClusterInit() throws InterruptedException {
+		ClusterHealthStatus status = null;
+		do {
+			Thread.sleep(2000);
+			status = client.admin().cluster().prepareHealth().get().getStatus();
+		} while (ClusterHealthStatus.RED.equals(status));
+	}
+
+	private void removeReplicas() {
+		client.admin().indices().prepareUpdateSettings("*")//
+				.setSettings(Settings.builder().put("number_of_replicas", 0).build())//
+				.get();
+	}
+
 	private void migrate() throws IOException {
 		ImmutableOpenMap<String, ImmutableOpenMap<String, MappingMetaData>> mappings = client.admin().indices()
 				.prepareGetMappings("*").get().getMappings();
 
+		log("Mappings: %s", mappings);
+
 		Iterator<String> indices = mappings.keysIt();
 		while (indices.hasNext()) {
-			String index = indices.next();
-			Iterator<String> types = mappings.get(index).keysIt();
-			while (types.hasNext()) {
-				String type = types.next();
-				MappingMetaData mapping = mappings.get(index).get(type);
-				elastic.createIndex(index, type, mapping.source().string(), 1, 1);
-				SearchResponse response = client.prepareSearch(index).setTypes(type).setSize(100).setScroll("1m").get();
-				reindex(index, type, response.getHits());
-				String scrollId = response.getScrollId();
-				do {
-					response = client.prepareSearchScroll(scrollId).setScroll("1m").get();
-					reindex(index, type, response.getHits());
-				} while (response.getHits().getHits().length < 100);
+			String backendId = indices.next();
+			// indices without any '-' are plain old backend indices
+			if (backendId.indexOf('-') < 0) {
+				Iterator<String> types = mappings.get(backendId).keysIt();
+				while (types.hasNext()) {
+					String type = types.next();
+					MappingMetaData mapping = mappings.get(backendId).get(type);
+					SchemaIndex index = new SchemaIndex(backendId, type, mapping.source().string());
+					index.migrate();
+				}
+			}
+		}
+	}
+
+	public class SchemaIndex {
+
+		private String backendId;
+		private String type;
+		private String mapping;
+		private int reindexed;
+
+		public SchemaIndex(String backendId, String type, String mapping) {
+			this.backendId = backendId;
+			this.type = type;
+			this.mapping = mapping;
+		}
+
+		public void migrate() {
+			ObjectNode json = Json.readObjectNode(mapping);
+			// remove any _id mapping field since it is not allowed
+			// in 2.x mappings
+			((ObjectNode) json.get(type)).remove("_id");
+
+			try {
+				elastic.createIndex(backendId, type, json.toString(), //
+						AbstractResource.SHARDS_DEFAULT, //
+						AbstractResource.REPLICAS_DEFAULT);
+			} catch (IndexAlreadyExistsException e) {
+				// TODO ignored for testing but to be removed
+				// for the final tests and production
 			}
 
+			SearchResponse response = client.prepareSearch(backendId)//
+					.setTypes(type).setSize(100).setScroll(TimeValue.timeValueMinutes(1))//
+					.setQuery(QueryBuilders.matchAllQuery()).get();
+			reindex(response.getHits());
+			String scrollId = response.getScrollId();
+			do {
+				response = client.prepareSearchScroll(scrollId)//
+						.setScroll(TimeValue.timeValueMinutes(1)).get();
+				reindex(response.getHits());
+			} while (response.getHits().getHits().length == 100);
+
+			log("/%s/%s => %s reindexed", elastic.toAlias(backendId, type), type, reindexed);
+		}
+
+		void reindex(SearchHits hits) {
+			if (hits.getHits().length > 0) {
+				BulkRequestBuilder request = client.prepareBulk();
+				for (SearchHit hit : hits) {
+					// elastic.index(index, type, hit.getId(), hit.source());
+					request.add(client.prepareIndex(elastic.toAlias(backendId, type), //
+							type, hit.getId()).setSource(hit.source()));
+				}
+
+				BulkItemResponse[] responses = request.get().getItems();
+				for (BulkItemResponse response : responses) {
+					if (response.isFailed())
+						log("Failure /%s/%s/%s => %s", response.getIndex(), response.getType(), //
+								response.getId(), response.getFailure().getCause());
+					else
+						reindexed++;
+				}
+			}
 		}
 	}
 
-	void reindex(String index, String type, SearchHits hits) {
-		for (SearchHit hit : hits) {
-			elastic.index(index, type, hit.getId(), hit.source());
-			log("%s\t\t%s\t\t%s", index, type, hit.getId());
-		}
-	}
-
-	private void log(String string, Object... args) {
+	private static void log(String string, Object... args) {
 		System.out.println(String.format(string, args));
 	}
 
