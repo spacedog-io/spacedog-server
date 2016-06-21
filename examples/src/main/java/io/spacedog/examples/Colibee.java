c4a0b587b1a2e18b71b0a6ec20fbaae7b9f51f90
David Attias <attias666@gmail.com>
2016-06-22 00:32:41 +0200

    Adds more schemas to colibee

--- examples/src/main/java/io/spacedog/examples/Colibee.java
+++ examples/src/main/java/io/spacedog/examples/Colibee.java
@@ -10,6 +10,7 @@ import com.google.common.io.Resources;
 
 import io.spacedog.client.SpaceClient;
 import io.spacedog.client.SpaceRequest;
+import io.spacedog.client.SpaceTarget;
 import io.spacedog.utils.Json;
 import io.spacedog.utils.JsonGenerator;
 import io.spacedog.utils.SchemaBuilder3;
@@ -17,17 +18,17 @@ import io.spacedog.utils.SchemaBuilder3;
 public class Colibee extends SpaceClient {
 
 	private ObjectNode schemaConsultant;
-	private ObjectNode schemaExpertise;
 	private ObjectNode schemaOpportunite;
+	private ObjectNode schemaGroupe;
+	private ObjectNode schemaRdv;
+	private ObjectNode schemaDiscussion;
+	private ObjectNode schemaMessage;
 
 	private User william;
 	private Backend backend;
 	private JsonGenerator generator;
 
 	public Colibee() throws Exception {
-		schemaConsultant = buildConsultantSchema();
-		schemaExpertise = buildExpertiseSchema();
-		schemaOpportunite = buildOpportuniteSchema();
 		backend = new Backend("colibee", "colibee", "hi colibee", "david@spacedog.io");
 		generator = new JsonGenerator();
 	}
@@ -35,24 +36,22 @@ public class Colibee extends SpaceClient {
 	@Test
 	public void initColibeeBackend() throws Exception {
 
-		// SpaceRequest.configuration().target(SpaceTarget.production);
+		SpaceRequest.configuration().target(SpaceTarget.production);
 
 		resetBackend(backend);
 		initUserDefaultSchema(backend);
 		william = createUser(backend, "william", "hi willian");
-		setSchema(schemaConsultant, backend);
-		setSchema(schemaExpertise, backend);
 
 		initGenerator();
+		initReferences();
 		initConsultantWilliam();
-		initExpertises();
+		initOpportuniteAirbus();
+		initGroupeFinance();
+		initRdvWilliam();
+		initDiscussionBale3();
 	}
 
 	private void initGenerator() throws Exception {
-
-		generator.reg("identite.prenom", "William");
-		generator.reg("identite.nom", "Miramond", "Lopez");
-		generator.reg("identite.titre", "Chef de projet", "Directeur des achats");
 
 		byte[] bytes = Resources.toByteArray(//
 				Resources.getResource("io/spacedog/examples/consultant.png"));
@@ -64,30 +63,96 @@ public class Colibee extends SpaceClient {
 	}
 
 	private void initConsultantWilliam() throws Exception {
+		schemaConsultant = buildConsultantSchema();
+		setSchema(schemaConsultant, backend);
 
-		ObjectNode consultant = generator.gen(schemaConsultant);
-
+		ObjectNode consultantWilliam = generator.gen(schemaConsultant);
 		SpaceRequest.post("/1/data/consultant?id=william")//
-				.userAuth(william).body(consultant).go(201);
+				.userAuth(william).body(consultantWilliam).go(201);
 	}
 
-	private void initExpertises() throws Exception {
+	private void initOpportuniteAirbus() throws Exception {
+		schemaOpportunite = buildOpportuniteSchema();
+		setSchema(schemaOpportunite, backend);
+		ObjectNode opportuniteAirbus = generator.gen(schemaOpportunite);
+		SpaceRequest.post("/1/data/opportunite?id=airbus")//
+				.userAuth(william).body(opportuniteAirbus).go(201);
+	}
 
-		ObjectNode expertise = Json.object();
+	private void initGroupeFinance() throws Exception {
+		schemaGroupe = buildGroupeSchema();
+		setSchema(schemaGroupe, backend);
+		ObjectNode groupeFinance = generator.gen(schemaGroupe);
+		SpaceRequest.post("/1/data/groupe?id=finance")//
+				.userAuth(william).body(groupeFinance).go(201);
+	}
 
-		expertise.set("secteurs", Json.object("asset-management", "Asset Management", //
+	private void initRdvWilliam() throws Exception {
+		schemaRdv = buildRdvSchema();
+		setSchema(schemaRdv, backend);
+		ObjectNode rdvWilliam = generator.gen(schemaRdv);
+		SpaceRequest.post("/1/data/rdv?id=william")//
+				.userAuth(william).body(rdvWilliam).go(201);
+	}
+
+	private void initDiscussionBale3() throws Exception {
+		schemaDiscussion = buildDiscussionSchema();
+		setSchema(schemaDiscussion, backend);
+		ObjectNode bale3 = generator.gen(schemaDiscussion);
+		String discussionId = SpaceRequest.post("/1/data/discussion?id=bale3")//
+				.userAuth(william).body(bale3).go(201).getFromJson("id").asText();
+
+		schemaMessage = buildMessageSchema();
+		setSchema(schemaMessage, backend);
+		ObjectNode message = generator.gen(schemaMessage);
+		message.put("discussionId", discussionId);
+		SpaceRequest.post("/1/data/message")//
+				.userAuth(william).body(message).go(201);
+
+	}
+
+	private void initReferences() throws Exception {
+
+		ObjectNode references = Json.object();
+
+		references.set("secteurs", Json.object("asset-management", "Asset Management", //
 				"assurance-vie", "Assurance vie", "automobile", "Automobile"));
 
-		expertise.set("methodes", Json.object("agile-scrum", "Agile / SCRUM", "audit", //
+		references.set("methodes", Json.object("agile-scrum", "Agile / SCRUM", "audit", //
 				"Audit", "big-data", "Big Data", "cadrage", "Cadrage"));
 
-		expertise.set("outils", Json.object("ariba", "Ariba", "essbase", "Essbase", //
+		references.set("outils", Json.object("ariba", "Ariba", "essbase", "Essbase", //
 				"hfm", "HFM", "hp", "HP"));
 
-		expertise.set("fonctions", Json.object("achats", "Achats", "acturiat", "Acturiat", //
-				"derivatives", "Derivatives"));
+		references.set("fonctions", Json.object("achat-bien-service", "Achats | biens & services", //
+				"fsi-acturiat", "FSI | Acturiat", "fsi-derivative", "FSI | Derivatives"));
 
-		SpaceRequest.post("/1/data/expertise?id=all").adminAuth(backend).body(expertise).go(201);
+		references.set("langues", Json.object("fr", "Français", "en", "Anglais", "de", "Allemand"));
+
+		references.set("typesPrestation",
+				Json.object("management-transition", "Management de transition", //
+						"Conseil en stratégie", "conseil-strategie", //
+						"conseil-management", "Conseil en management", //
+						"conseil-système-information", "Conseil en systèmes d'information", //
+						"assistance-maitrise-oeuvre", "Assistance à maîtrise d'œuvre"));
+
+		references.set("rythmesMission", Json.object("temps-plein", "Temps plein", //
+				"temps-partiel", "Temps partiel"));
+
+		references.set("deplacements", Json.object("aucun", "Aucun", "ponctuel", "Ponctuels", //
+				"regulier", "Réguliers"));
+
+		references.set("statutOpportunite", Json.object("identifie", "Identifiée", "ouverte", "Ouverte", //
+				"ferme", "Fermée"));
+
+		references.set("resultatOpportunite", Json.object("en-cours", "En cours", "gagnee", "Gagnée", //
+				"perdue", "Perdue", "gelee", "Gelée", "annulee", "Annulée"));
+
+		references.set("motifPerdue", Json.object("prix", "Prix", "pas-de-profil", "Pas de profil", //
+				"profil-non-retenu", "Profil non retenu"));
+
+		SpaceRequest.put("/1/stash").adminAuth(backend).go(200);
+		SpaceRequest.put("/1/stash/references").adminAuth(backend).body(references).go(201);
 	}
 
 	static ObjectNode buildConsultantSchema() {
@@ -174,17 +239,86 @@ public class Colibee extends SpaceClient {
 				.build();
 	}
 
-	static ObjectNode buildExpertiseSchema() {
-		return SchemaBuilder3.builder("expertise") //
-				.stash("fonctions")//
-				.stash("outils")//
-				.stash("secteurs")//
-				.stash("methodes")//
+	private ObjectNode buildOpportuniteSchema() {
+		return SchemaBuilder3.builder("opportunite") //
+				.text("titre").french().examples("Audit financier")//
+				.text("contexte").french()//
+				.text("objectif").french()//
+				.text("contribution").french()//
+				.text("commentaires").french()//
+				.bool("public")//
+				.string("statut")//
+				.string("resultat").examples("En cours")//
+				.string("typePrestation").array().examples("Conseil en stratégie")//
+				.string("secteurActivite").examples("Assurance vie")//
+				.string("fonctions").array().examples("Achats | biens & services")//
+				.string("methodes").array().examples("Audit")//
+				.string("outils").array().examples("Ariba")//
+				.string("langues").array().examples("Français", "Anglais")//
+
+				.text("ville").french().examples("Boulogne Billancourt")//
+				.string("pays").examples("FR")//
+				.date("demarreLe").examples("2016-09-10")//
+				.date("termineLe").examples("2016-12-24")//
+				.integer("charge").examples(25)//
+				.integer("rythme").examples(3)//
+				.string("deplacement").examples("Ponctuels")//
+				.object("client")//
+				.text("raisonSociale").french().examples("Airbus")//
+				.text("nom").french().examples("Jérôme")//
+				.text("prenom").french().examples("Dupont")//
+				.string("tel").examples("+33 6 62 78 34 56")//
+				.string("email").examples("jdupont@airbus.com")//
+				.close()//
+				.timestamp("identifieeLe").examples("2016-07-01T14:00:00.000Z")//
+				.timestamp("ouverteLe").examples("2016-07-01T14:00:00.000Z")//
+				.timestamp("fermeeLe").examples("2016-07-01T14:00:00.000Z")//
+				.string("motifPerdue").examples("Prix")//
 				.build();
 	}
 
-	private ObjectNode buildOpportuniteSchema() {
-		return SchemaBuilder3.builder("opportunite") //
+	private ObjectNode buildGroupeSchema() {
+		return SchemaBuilder3.builder("groupe") //
+				.text("titre").french()//
+				.text("description").french()//
+				.bool("membreOnly")//
+				.string("animateur")//
+				.string("consultants").array()//
+				.build();
+
+	}
+
+	private ObjectNode buildRdvSchema() {
+		return SchemaBuilder3.builder("rdv") //
+				.text("titre").french()//
+				.string("categorie").examples("Présentation client")//
+				.timestamp("debut")//
+				.timestamp("fin")//
+				.text("lieu")//
+				.string("participants").array()//
+				.text("commentaires")//
+				// animateur = owner of object
+				.build();
+	}
+
+	private ObjectNode buildDiscussionSchema() {
+		return SchemaBuilder3.builder("discussion") //
+				.text("titre").french()//
+				.string("statut").examples("open", "close")//
+				.timestamp("fermeeLe")//
+				.string("fermeePar")//
+				.build();
+	}
+
+	private ObjectNode buildMessageSchema() {
+		return SchemaBuilder3.builder("message") //
+				.text("texte").french()//
+				.string("discussionId")//
+				.object("reponses")//
+				.text("texte").french()//
+				.timestamp("ecritLe")//
+				.string("ecritPar")//
+				.close()//
 				.build();
 	}
 }
