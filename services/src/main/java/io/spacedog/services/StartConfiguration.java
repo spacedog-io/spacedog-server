601e3879c8de71e1e3327759fd028382d16b301d
David Attias <attias666@gmail.com>
2016-10-14 18:15:58 +0200

    Improves Start configuration logging

--- services/src/main/java/io/spacedog/services/StartConfiguration.java
+++ services/src/main/java/io/spacedog/services/StartConfiguration.java
@@ -15,11 +15,27 @@ import io.spacedog.utils.Utils;
 
 public class StartConfiguration {
 
+	private static final String SPACEDOG_HOME_PATH = "spacedog.home.path";
+	private static final String SPACEDOG_CONFIGURATION_FILE = "spacedog.configuration.file";
+	private static final String SPACEDOG_ELASTIC_HTTP_ENABLED = "spacedog.elastic.http.enabled";
+	private static final String SPACEDOG_OFFLINE = "spacedog.offline";
+	private static final String SPACEDOG_AWS_SUPERDOG_NOTIFICATION_TOPIC = "spacedog.aws.superdog.notification.topic";
+	private static final String SPACEDOG_ELASTIC_SNAPSHOTS_PATH = "spacedog.elastic.snapshots.path";
+	private static final String SPACEDOG_MAIL_SMTP_DEBUG = "spacedog.mail.smtp.debug";
+	private static final String SPACEDOG_MAIL_DOMAIN = "spacedog.mail.domain";
+	private static final String SPACEDOG_MAIL_MAILGUN_KEY = "spacedog.mail.mailgun.key";
+	private static final String SPACEDOG_AWS_BUCKET_PREFIX = "spacedog.aws.bucket.prefix";
+	private static final String SPACEDOG_AWS_REGION = "spacedog.aws.region";
+	private static final String SPACEDOG_PRODUCTION = "spacedog.production";
+	private static final String SPACEDOG_ELASTIC_DATA_PATH = "spacedog.elastic.data.path";
+	private static final String SPACEDOG_SERVER_PORT = "spacedog.server.port";
+	private static final String SPACEDOG_API_URL_SCHEME = "spacedog.api.url.scheme";
+	private static final String SPACEDOG_API_URL_BASE = "spacedog.api.url.base";
 	private Properties configuration = new Properties();
 
 	public StartConfiguration() {
 
-		checkPath("configuration file", configFilePath(), false);
+		checkPath(SPACEDOG_CONFIGURATION_FILE, configFilePath(), false);
 
 		try {
 			configuration.load(Files.newInputStream(configFilePath()));
@@ -28,24 +44,24 @@ public class StartConfiguration {
 					"error loading [%s] configuration file", configFilePath());
 		}
 
-		checkPath("home path", homePath(), true);
-		check("production", isProduction());
-		check("offline", isOffline());
-		check("server port", serverPort());
+		checkPath(SPACEDOG_HOME_PATH, homePath(), true);
+		check(SPACEDOG_PRODUCTION, isProduction());
+		check(SPACEDOG_OFFLINE, isOffline());
+		check(SPACEDOG_SERVER_PORT, serverPort());
 
-		check("api url", apiUrl());
+		check("API URL", apiUrl());
 
-		checkPath("elastic data path", elasticDataPath(), true);
-		check("elastic http enabled", isElasticHttpEnabled());
-		checkPath("elastic snapshots path", snapshotsPath(), true);
+		checkPath(SPACEDOG_ELASTIC_DATA_PATH, elasticDataPath(), true);
+		check(SPACEDOG_ELASTIC_HTTP_ENABLED, isElasticHttpEnabled());
+		checkPath(SPACEDOG_ELASTIC_SNAPSHOTS_PATH, snapshotsPath(), true);
 
-		check("AWS region", awsRegion());
-		check("AWS bucket prefix", getAwsBucketPrefix());
-		check("AWS superdog notification topic", superdogAwsNotificationTopic());
+		check(SPACEDOG_AWS_REGION, awsRegion());
+		check(SPACEDOG_AWS_BUCKET_PREFIX, getAwsBucketPrefix());
+		check(SPACEDOG_AWS_SUPERDOG_NOTIFICATION_TOPIC, superdogAwsNotificationTopic());
 
-		check("mail domain", mailDomain());
-		check("mail smtp debug", mailSmtpDebug());
-		check("mailgun key", mailGunKey());
+		check(SPACEDOG_MAIL_DOMAIN, mailDomain());
+		check(SPACEDOG_MAIL_SMTP_DEBUG, mailSmtpDebug());
+		check(SPACEDOG_MAIL_MAILGUN_KEY, mailGunKey());
 
 		if (isProduction()) {
 			// Force Fluent HTTP to production mode
@@ -55,14 +71,14 @@ public class StartConfiguration {
 	}
 
 	public Path homePath() {
-		String path = configuration.getProperty("spacedog.home.path");
+		String path = configuration.getProperty(SPACEDOG_HOME_PATH);
 		return path == null //
 				? Paths.get(System.getProperty("user.home"), "spacedog")//
 				: Paths.get(path);
 	}
 
 	public Path configFilePath() {
-		String path = System.getProperty("spacedog.configuration.file");
+		String path = System.getProperty(SPACEDOG_CONFIGURATION_FILE);
 		return path == null //
 				? homePath().resolve("spacedog.server.properties")//
 				: Paths.get(path);
@@ -80,63 +96,67 @@ public class StartConfiguration {
 	}
 
 	public String apiUrlBase() {
-		return configuration.getProperty("spacedog.api.url.base");
+		return configuration.getProperty(SPACEDOG_API_URL_BASE);
 	}
 
 	public String apiUrlScheme() {
-		return configuration.getProperty("spacedog.api.url.scheme");
+		return configuration.getProperty(SPACEDOG_API_URL_SCHEME);
 	}
 
 	public int serverPort() {
-		return Integer.valueOf(configuration.getProperty("spacedog.server.port"));
+		return Integer.valueOf(configuration.getProperty(SPACEDOG_SERVER_PORT));
 	}
 
 	public Path elasticDataPath() {
-		String path = configuration.getProperty("spacedog.elastic.data.path");
+		String path = configuration.getProperty(SPACEDOG_ELASTIC_DATA_PATH);
 		return path == null ? //
 				homePath().resolve("data") : Paths.get(path);
 	}
 
 	public boolean isProduction() {
-		return Boolean.parseBoolean(configuration.getProperty("spacedog.production", "false"));
+		return Boolean.parseBoolean(//
+				configuration.getProperty(SPACEDOG_PRODUCTION, "false"));
 	}
 
 	public String awsRegion() {
-		return configuration.getProperty("spacedog.aws.region");
+		return configuration.getProperty(SPACEDOG_AWS_REGION);
 	}
 
 	public String getAwsBucketPrefix() {
-		return configuration.getProperty("spacedog.aws.bucket.prefix");
+		return configuration.getProperty(SPACEDOG_AWS_BUCKET_PREFIX);
 	}
 
 	public String mailGunKey() {
-		return configuration.getProperty("spacedog.mail.mailgun.key");
+		return configuration.getProperty(SPACEDOG_MAIL_MAILGUN_KEY);
 	}
 
 	public String mailDomain() {
-		return configuration.getProperty("spacedog.mail.domain");
+		return configuration.getProperty(SPACEDOG_MAIL_DOMAIN);
 	}
 
 	public boolean mailSmtpDebug() {
-		return Boolean.parseBoolean(configuration.getProperty("spacedog.mail.smtp.debug", "false"));
+		return Boolean.parseBoolean(//
+				configuration.getProperty(SPACEDOG_MAIL_SMTP_DEBUG, "false"));
 	}
 
 	public Optional<Path> snapshotsPath() {
-		String path = configuration.getProperty("spacedog.elastic.snapshots.path");
+		String path = configuration.getProperty(SPACEDOG_ELASTIC_SNAPSHOTS_PATH);
 		return Strings.isNullOrEmpty(path) ? Optional.empty()//
 				: Optional.of(Paths.get(path));
 	}
 
 	public String superdogAwsNotificationTopic() {
-		return configuration.getProperty("spacedog.aws.superdog.notification.topic");
+		return configuration.getProperty(SPACEDOG_AWS_SUPERDOG_NOTIFICATION_TOPIC);
 	}
 
 	public boolean isOffline() {
-		return Boolean.parseBoolean(configuration.getProperty("spacedog.offline", "false"));
+		return Boolean.parseBoolean(//
+				configuration.getProperty(SPACEDOG_OFFLINE, "false"));
 	}
 
 	public boolean isElasticHttpEnabled() {
-		return Boolean.parseBoolean(configuration.getProperty("spacedog.elastic.http.enabled", "false"));
+		return Boolean.parseBoolean(//
+				configuration.getProperty(SPACEDOG_ELASTIC_HTTP_ENABLED, "false"));
 	}
 
 	//
@@ -146,10 +166,10 @@ public class StartConfiguration {
 	private void check(String property, Object value) {
 
 		if (value == null || value.toString().isEmpty())
-			throw new IllegalArgumentException(String.format(//
-					"Configuration setting [%s] is required", property));
+			throw Exceptions.illegalArgument(//
+					"Configuration setting [%s] is required", property);
 
-		Utils.info("[SpaceDog] setting %s = %s", property, value);
+		Utils.info("[SpaceDog] %s = %s", property, value);
 	}
 
 	private void checkPath(String property, Optional<Path> path, boolean directory) {
@@ -160,18 +180,18 @@ public class StartConfiguration {
 	private void checkPath(String property, Path path, boolean directory) {
 
 		if (path == null)
-			throw new IllegalArgumentException(String.format(//
-					"Configuration setting [%s] is required", property));
+			throw Exceptions.illegalArgument(//
+					"SpaceDog configuration [%s][%s] is required", property, path);
 
 		if (directory && !Files.isDirectory(path))
-			throw new IllegalArgumentException(String.format(//
-					"Configuration setting [%s] is not a directory", property));
+			throw Exceptions.illegalArgument(//
+					"SpaceDog configuration [%s][%s] is not a directory", property, path);
 
 		if (!directory && !Files.isReadable(path))
-			throw new IllegalArgumentException(String.format(//
-					"Configuration setting [%s] is not a readable file", property));
+			throw Exceptions.illegalArgument(//
+					"SpaceDog configuration [%s][%s] is not a readable file", property, path);
 
-		Utils.info("[SpaceDog] setting %s = %s", property, path);
+		Utils.info("[SpaceDog] %s = %s", property, path);
 	}
 
 }
