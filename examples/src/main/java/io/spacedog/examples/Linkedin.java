347ab15d4cc17ccc6770f1ec2d4770501ac7374f
David Attias <attias666@gmail.com>
2016-10-15 00:15:03 +0200

    Improves /login/linkedin testing

--- examples/src/main/java/io/spacedog/examples/Linkedin.java
+++ examples/src/main/java/io/spacedog/examples/Linkedin.java
@@ -3,6 +3,10 @@
  */
 package io.spacedog.examples;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.apache.http.client.utils.URIBuilder;
 import org.junit.Test;
 
 import io.spacedog.client.SpaceClient;
@@ -11,29 +15,7 @@ import io.spacedog.utils.CredentialsSettings;
 
 public class Linkedin extends SpaceClient {
 
-	@Test
-	public void linkedinLoginWITHOUTPreRegisteredCredentialsByAdmin() {
-
-		// prepare
-		SpaceClient.prepareTest();
-		Backend test = SpaceClient.resetTestBackend();
-
-		// credentials settings with guest sign up enabled
-		CredentialsSettings settings = setCredentialsSettings(test, false, false);
-
-		// start linkedin authentication process to display
-		// url to call inside the browser for testing
-
-		SpaceRequest//
-				.get("https://www.linkedin.com/oauth/v2/authorization")//
-				.queryParam("response_type", "code")//
-				.queryParam("state", "thisIsSpaceDog")//
-				.queryParam("redirect_uri", settings.linkedinRedirectUri)//
-				.queryParam("client_id", settings.linkedinId)//
-				.go(200, 303);
-	}
-
-	@Test
+	// @Test
 	public void getMyLinkedinProfil() {
 
 		// prepare
@@ -47,70 +29,141 @@ public class Linkedin extends SpaceClient {
 	}
 
 	@Test
-	public void linkedinLoginWITHPreRegisteredCredentialsByAdmin() {
-
-		// prepare
-		SpaceClient.prepareTest();
-		Backend test = SpaceClient.resetTestBackend();
-
-		// set credentials settings with guest sign up disabled
-		CredentialsSettings settings = setCredentialsSettings(test, true, false);
-
-		// admin pre registers some credentials for a new user
-		// username must equals the linkedin account email
-		String resetCode = SpaceRequest.post("/1/credentials").adminAuth(test)//
-				.body("username", "attias666@gmail.com", "email", "attias666@gmail.com")//
-				.go(201).getString("passwordRestCode");
-
-		// start linkedin authentication process to display
-		// url to call inside the browser for testing
-
-		SpaceRequest//
-				.get("https://www.linkedin.com/oauth/v2/authorization")//
-				.queryParam("response_type", "code")//
-				.queryParam("state", resetCode)//
-				.queryParam("redirect_uri", settings.linkedinRedirectUri)//
-				.queryParam("client_id", settings.linkedinId)//
-				.go(200, 303);
-	}
-
-	@Test
-	public void linkedinRedirectedLogin() {
+	public void login() //
+			throws URISyntaxException, IOException {
 
 		// prepare
 		SpaceClient.prepareTest();
 		Backend test = SpaceClient.resetTestBackend();
 
 		// credentials settings with guest sign up enabled
-		// and redirected login url
-		CredentialsSettings settings = setCredentialsSettings(test, false, true);
+		CredentialsSettings settings = defaultCredentialsSettings(false);
+		SpaceClient.saveSettings(test, settings);
 
-		// start linkedin authentication process to display
-		// url to call inside the browser for testing
-
-		SpaceRequest//
-				.get("https://www.linkedin.com/oauth/v2/authorization")//
-				.queryParam("response_type", "code")//
-				.queryParam("state", "thisIsSpaceDog")//
-				.queryParam("redirect_uri", settings.linkedinRedirectUri)//
-				.queryParam("client_id", settings.linkedinId)//
-				.go(200, 303);
+		// login succeeds
+		linkedinLogin(test, false, settings);
 	}
 
-	private CredentialsSettings setCredentialsSettings(Backend backend, boolean disableGuestSignUp,
-			boolean redirectLogin) {
-		CredentialsSettings settings = new CredentialsSettings();
+	@Test
+	public void loginFailsCauseGuestSignUpIsDisabled() //
+			throws URISyntaxException, IOException {
 
+		// prepare
+		SpaceClient.prepareTest();
+		Backend test = SpaceClient.resetTestBackend();
+
+		// set credentials settings with guest sign up disabled
+		CredentialsSettings settings = defaultCredentialsSettings(true);
+		SpaceClient.saveSettings(test, settings);
+
+		// login fails since guest sign in is disabled
+		// and credentials has not been pre created by admin
+		linkedinLogin(test, false, settings);
+	}
+
+	@Test
+	public void loginSucceedsCauseCredentialsPreRegisteredByAdmin() //
+			throws URISyntaxException, IOException {
+
+		// prepare
+		SpaceClient.prepareTest();
+		Backend test = SpaceClient.resetTestBackend();
+
+		// set credentials settings with guest sign up disabled
+		CredentialsSettings settings = defaultCredentialsSettings(true);
+		SpaceClient.saveSettings(test, settings);
+
+		// admin pre registers some credentials for a new user
+		// username must equals the linkedin account email
+		SpaceRequest.post("/1/credentials").adminAuth(test)//
+				.body("username", "attias666@gmail.com", "email", "attias666@gmail.com")//
+				.go(201).assertPresent("passwordResetCode");
+
+		// login succeeds
+		// check credentialsId in json payload is identical
+		// to credentialsId in previous request before login
+		linkedinLogin(test, false, settings);
+	}
+
+	@Test
+	public void loginFailsCauseInvalidSecret() //
+			throws URISyntaxException, IOException {
+
+		// prepare
+		SpaceClient.prepareTest();
+		Backend test = SpaceClient.resetTestBackend();
+
+		// set credentials settings with guest sign up disabled
+		CredentialsSettings settings = defaultCredentialsSettings(true);
+		settings.linkedinSecret = "XXX";
+		SpaceClient.saveSettings(test, settings);
+
+		// login fails since secret is invalid
+		// check error in response json payload
+		linkedinLogin(test, false, settings);
+	}
+
+	@Test
+	public void redirectedLogin() throws URISyntaxException, IOException {
+
+		// prepare
+		SpaceClient.prepareTest();
+		Backend test = SpaceClient.resetTestBackend();
+
+		// credentials settings with guest sign up enabled
+		CredentialsSettings settings = defaultCredentialsSettings(false);
+		SpaceClient.saveSettings(test, settings);
+
+		// login succeeds
+		// check access token in redirect url params
+		linkedinLogin(test, true, settings);
+	}
+
+	@Test
+	public void redirectedLoginFailsCauseInvalidSecret() //
+			throws URISyntaxException, IOException {
+
+		// prepare
+		SpaceClient.prepareTest();
+		Backend test = SpaceClient.resetTestBackend();
+
+		// set credentials settings with guest sign up disabled
+		CredentialsSettings settings = defaultCredentialsSettings(true);
+		settings.linkedinSecret = "XXX";
+		SpaceClient.saveSettings(test, settings);
+
+		// login fails since secret is invalid
+		// check error in redirect url params
+		linkedinLogin(test, true, settings);
+	}
+
+	private void linkedinLogin(Backend backend, boolean redirect, CredentialsSettings settings)
+			throws URISyntaxException, IOException {
+
+		// test redirect uri
+		String redirectUri = SpaceRequest.configuration().target()//
+				.url(backend.backendId, "/1/login/linkedin");
+
+		if (redirect)
+			redirectUri = redirectUri + "/redirect";
+
+		// build the linkedin oauth2 signin url
+		URIBuilder url = new URIBuilder("https://www.linkedin.com/oauth/v2/authorization");
+		url.addParameter("response_type", "code");
+		url.addParameter("state", "thisIsSpaceDog");
+		url.addParameter("redirect_uri", redirectUri);
+		url.addParameter("client_id", settings.linkedinId);
+
+		// open a browser with the linkedin signin url
+		// to start the linkedin oauth2 authentication process
+		Runtime.getRuntime().exec("open " + url.toString());
+	}
+
+	private CredentialsSettings defaultCredentialsSettings(boolean disableGuestSignUp) {
+		CredentialsSettings settings = new CredentialsSettings();
 		settings.disableGuestSignUp = disableGuestSignUp;
 		settings.linkedinId = SpaceRequest.configuration().testLinkedinClientId();
 		settings.linkedinSecret = SpaceRequest.configuration().testLinkedinClientSecret();
-		settings.linkedinRedirectUri = SpaceRequest.configuration().target()//
-				.url(backend.backendId, "/1/login/linkedin");
-
-		if (redirectLogin)
-			settings.linkedinRedirectUri = settings.linkedinRedirectUri + "/redirect";
-
-		SpaceClient.saveSettings(backend, settings);
 		return settings;
 	}
 }
--- services/src/main/java/io/spacedog/services/LinkedinResource.java
+++ services/src/main/java/io/spacedog/services/LinkedinResource.java
@@ -4,6 +4,8 @@ import org.joda.time.DateTime;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.base.Strings;
+import com.google.common.escape.Escaper;
+import com.google.common.net.UrlEscapers;
 
 import io.spacedog.client.SpaceRequest;
 import io.spacedog.client.SpaceResponse;
@@ -13,6 +15,7 @@ import io.spacedog.utils.Credentials.Level;
 import io.spacedog.utils.CredentialsSettings;
 import io.spacedog.utils.Exceptions;
 import io.spacedog.utils.JsonBuilder;
+import io.spacedog.utils.SpaceException;
 import io.spacedog.utils.SpaceHeaders;
 import io.spacedog.utils.Utils;
 import net.codestory.http.Context;
@@ -21,7 +24,7 @@ import net.codestory.http.annotations.Post;
 import net.codestory.http.constants.HttpStatus;
 import net.codestory.http.payload.Payload;
 
-public class LinkedinResource {
+public class LinkedinResource extends Resource {
 
 	//
 	// Routes
@@ -51,26 +54,47 @@ public class LinkedinResource {
 	@Get("/1/login/linkedin/redirect/")
 	public Payload getRedirectLogin(Context context) {
 
-		Credentials credentials = login(context);
-		CredentialsSettings settings = SettingsResource.get().load(CredentialsSettings.class);
-
 		String redirectUri = context.get("redirect_uri");
-		if (Strings.isNullOrEmpty(redirectUri))
-			redirectUri = settings.linkedinRedirectUri;
-		Check.notNullOrEmpty(redirectUri, "redirect_uri");
 		String state = context.get("state");
-		Check.notNullOrEmpty(state, "state");
 
-		// TODO remove this when mikael finds out why
-		// the redirect_uri is passed with a ';' suffix
-		// in mobile app
-		redirectUri = Utils.removePreffix(redirectUri, ";");
+		if (Strings.isNullOrEmpty(redirectUri))
+			// no redirect_uri means this is a test
+			// redirect to this server root url
+			redirectUri = spaceRootUrl(SpaceContext.backendId()).toString();
+		else
+			// TODO remove this when mikael finds out why
+			// the redirect_uri is passed with a ';' suffix
+			// in mobile apps
+			redirectUri = Utils.removeSuffix(redirectUri, ";");
 
+		Escaper paramEscaper = UrlEscapers.urlPathSegmentEscaper();
 		StringBuilder location = new StringBuilder(redirectUri)//
-				.append("#state=").append(state)//
-				.append("&access_token=").append(credentials.accessToken())//
-				.append("&expires=").append(credentials.accessTokenExpiresIn())//
-				.append("&credentialsId=").append(credentials.id());
+				.append("#state=");
+
+		if (state != null)
+			location.append(paramEscaper.escape(state));
+
+		try {
+			Credentials credentials = login(context);
+
+			location.append("&access_token=")//
+					.append(paramEscaper.escape(credentials.accessToken()))//
+					.append("&expires=")//
+					.append(credentials.accessTokenExpiresIn())//
+					.append("&credentialsId=")//
+					.append(paramEscaper.escape(credentials.id()));
+
+		} catch (SpaceException e) {
+			location.append("&error=")//
+					.append(paramEscaper.escape(e.code()))//
+					.append("&error_description=")//
+					.append(paramEscaper.escape(e.getMessage()));
+
+		} catch (Throwable t) {
+			location.append("&error=internal-server-error")//
+					.append("&error_description=")//
+					.append(paramEscaper.escape(t.getMessage()));
+		}
 
 		return new Payload(302).withHeader(SpaceHeaders.LOCATION, location.toString());
 	}
@@ -97,14 +121,17 @@ public class LinkedinResource {
 	//
 
 	private void checkLinkedinError(SpaceResponse response, String messageIntro) {
+
 		if (response.httpResponse().getStatus() >= 400) {
+			StringBuilder message = new StringBuilder(messageIntro);
 
-			String details = response.has("error_description") //
-					? response.getString("error_description")//
-					: "no linkedin error description";
+			if (response.has("error"))
+				message.append(": ").append(response.getString("error"));
 
-			throw Exceptions.space(response.httpResponse().getStatus(), //
-					"%s: %s", messageIntro, details);
+			if (response.has("error_description"))
+				message.append(" (").append(response.getString("error_description")).append(")");
+
+			throw Exceptions.space(response.httpResponse().getStatus(), message.toString());
 		}
 	}
 
@@ -118,20 +145,21 @@ public class LinkedinResource {
 
 		CredentialsSettings settings = SettingsResource.get().load(CredentialsSettings.class);
 		if (Strings.isNullOrEmpty(settings.linkedinId))
-			throw Exceptions.illegalArgument("no linkedin client id found in credentials settings");
+			throw Exceptions.illegalArgument("credentials settings linkedin client id is required");
 
 		String redirectUri = context.get("redirect_uri");
-		if (Strings.isNullOrEmpty(redirectUri))
-			redirectUri = settings.linkedinRedirectUri;
-		Check.notNullOrEmpty(redirectUri, "redirect_uri");
 
-		// TODO remove this when mikael finds out why
-		// the redirect_uri is passed with a ';' suffix
-		// in mobile app
-		redirectUri = Utils.removePreffix(redirectUri, ";");
+		// no redirect_uri means this is a test
+		// in this case, redirect_uri = this service uri
+		if (Strings.isNullOrEmpty(redirectUri))
+			redirectUri = spaceUrl(backendId, context.uri()).toString();
+		else
+			// TODO remove this when mikael finds out why
+			// the redirect_uri is passed with a ';' suffix
+			// in mobile app
+			redirectUri = Utils.removeSuffix(redirectUri, ";");
 
 		DateTime expiresAt = DateTime.now();
-
 		SpaceResponse response = SpaceRequest//
 				.post("https://www.linkedin.com/oauth/v2/accessToken")//
 				.queryParam("grant_type", "authorization_code")//
