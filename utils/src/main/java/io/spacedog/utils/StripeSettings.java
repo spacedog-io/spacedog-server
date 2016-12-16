38ed4da6ebd246f1c6a3ed8840854961041354a3
David Attias <attias666@gmail.com>
2016-12-16 13:19:22 +0100

    Fixes missing stripe settings

--- utils/src/main/java/io/spacedog/utils/StripeSettings.java
+++ utils/src/main/java/io/spacedog/utils/StripeSettings.java
@@ -1,6 +1,10 @@
 package io.spacedog.utils;
 
+import java.util.Set;
+
 public class StripeSettings extends Settings {
 
 	public String secretKey;
+	public Set<String> rolesAllowedToCharge;
+	public Set<String> rolesAllowedToPay;
 }
