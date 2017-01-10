b5b88f91b43e29f88988a35e5d78b71038480516
David Attias <attias666@gmail.com>
2017-01-10 23:37:35 +0100

    Purge admin job now deletes 7 days old logs and is authenticated with root user with purgeall role

--- watchdog/src/main/java/io/spacedog/admin/Purge.java
+++ watchdog/src/main/java/io/spacedog/admin/Purge.java
@@ -1,5 +1,8 @@
 package io.spacedog.admin;
 
+import org.joda.time.DateTime;
+
+import io.spacedog.client.SpaceEnv;
 import io.spacedog.client.SpaceRequest;
 
 public class Purge {
@@ -10,9 +13,12 @@ public class Purge {
 			// set high timeout to wait for purge response from server
 			// since delete of thousands of logs might take long
 			SpaceRequest.env().httpTimeoutMillis(120000);
+			String password = SpaceEnv.defaultEnv().get("spacedog_jobs_purgeall_password");
+			String before = DateTime.now().minusDays(7).toString();
 
-			SpaceRequest.delete("/1/log?from=100000")//
-					.superdogAuth()//
+			SpaceRequest.delete("/1/log")//
+					.queryParam("before", before)//
+					.basicAuth("api", "purgeall", password)//
 					.go(200);
 
 			return AdminJobs.ok(this);
