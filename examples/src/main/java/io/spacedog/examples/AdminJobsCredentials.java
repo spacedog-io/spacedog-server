2249687a4b44bd29298059fba6f4021c4fe492a2
David Attias <attias666@gmail.com>
2017-01-15 22:49:55 +0100

    Improves AdminJobsCredentials

--- examples/src/main/java/io/spacedog/examples/AdminJobsCredentials.java
+++ examples/src/main/java/io/spacedog/examples/AdminJobsCredentials.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import io.spacedog.client.SpaceClient;
 import io.spacedog.client.SpaceEnv;
 import io.spacedog.client.SpaceRequest;
+import io.spacedog.client.SpaceTarget;
 import io.spacedog.services.LogResource;
 import io.spacedog.services.SnapshotResource;
 
@@ -12,38 +13,30 @@ public class AdminJobsCredentials extends SpaceClient {
 
 	@Test
 	public void initPurgeAllCredentials() {
-
-		// SpaceRequest.env().target(SpaceTarget.production);
-
+		SpaceRequest.env().target(SpaceTarget.production);
 		String password = SpaceEnv.defaultEnv().get("spacedog_jobs_purgeall_password");
-
-		SpaceClient.deleteCredentialsBySuperdog("api", LogResource.PURGE_ALL);
-
-		String id = SpaceRequest.post("/1/credentials").superdogAuth()//
-				.body(FIELD_USERNAME, LogResource.PURGE_ALL, FIELD_PASSWORD, password, //
-						FIELD_EMAIL, "platform@spacedog.io")
-				.go(201).getString(FIELD_ID);
-
-		SpaceRequest.put("/1/credentials/{id}/roles/" + LogResource.PURGE_ALL)//
-				.routeParam(FIELD_ID, id).superdogAuth().go(200);
+		initCredentials(LogResource.PURGE_ALL, password, LogResource.PURGE_ALL);
 	}
 
 	@Test
 	public void initSnapshotAllCredentials() {
 
-		// SpaceRequest.env().target(SpaceTarget.production);
-
+		SpaceRequest.env().target(SpaceTarget.production);
 		String password = SpaceEnv.defaultEnv().get("spacedog_jobs_snapshotall_password");
+		initCredentials(SnapshotResource.SNAPSHOT_ALL, password, SnapshotResource.SNAPSHOT_ALL);
+	}
 
-		SpaceClient.deleteCredentialsBySuperdog("api", SnapshotResource.SNAPSHOT_ALL);
+	private void initCredentials(String username, String password, String role) {
+
+		SpaceClient.deleteCredentialsBySuperdog("api", username);
 
 		String id = SpaceRequest.post("/1/credentials").superdogAuth()//
-				.body(FIELD_USERNAME, SnapshotResource.SNAPSHOT_ALL, //
-						FIELD_PASSWORD, password, FIELD_EMAIL, "platform@spacedog.io")
+				.body(FIELD_USERNAME, username, FIELD_PASSWORD, password, //
+						FIELD_EMAIL, "platform@spacedog.io")
 				.go(201).getString(FIELD_ID);
 
-		SpaceRequest.put("/1/credentials/{id}/roles/" + SnapshotResource.SNAPSHOT_ALL)//
-				.routeParam(FIELD_ID, id).superdogAuth().go(200);
+		SpaceRequest.put("/1/credentials/{id}/roles/{role}")//
+				.routeParam(FIELD_ID, id).routeParam("role", role).superdogAuth().go(200);
 	}
 
 }
