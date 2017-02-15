1f4effcb1cf3efdf4597bf090701b41abfc89ba6
David Attias <attias666@gmail.com>
2017-02-15 17:20:42 +0100

    Adds data migration script for Colibee preprod

---
+++ examples/src/main/java/io/spacedog/examples/colibee/InstallProdDataToPreprod.java
@@ -0,0 +1,61 @@
+/**
+ * © David Attias 2015
+ */
+package io.spacedog.examples.colibee;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import io.spacedog.client.SpaceRequest;
+import io.spacedog.client.SpaceTest;
+import io.spacedog.utils.Json;
+import io.spacedog.utils.Utils;
+
+public class InstallProdDataToPreprod extends SpaceTest {
+
+	@Test
+	public void run() {
+
+		// upgradeAliases();
+		upgradeCredentials();
+
+	}
+
+	void upgradeAliases() {
+
+		SpaceRequest
+				.post("http://connectapipreproduction.colibee.com:9200"//
+						+ "/_aliases")//
+				.bodyResource(this.getClass(), "aliases.json")//
+				.go(200);
+	}
+
+	void upgradeCredentials() {
+
+		ObjectNode query = Json.objectBuilder()//
+				.put("size", 400)//
+				.object("query")//
+				.object("term")//
+				.put("backendId", "connectapi")//
+				.build();
+
+		JsonNode node = SpaceRequest
+				.post("http://connectapipreproduction.colibee.com:9200"//
+						+ "/spacedog-credentials/credentials/_search")//
+				.body(query).go(200).get("hits.hits");
+
+		for (JsonNode credentials : node) {
+			String id = credentials.get("_id").asText();
+			Utils.info("Updating credentials [%s] ...", id);
+
+			SpaceRequest
+					.post("http://connectapipreproduction.colibee.com:9200"//
+							+ "/spacedog-credentials/credentials/" //
+							+ id + "/_update")//
+					.body("doc", Json.object("backendId", "connectapipreproduction"))//
+					.go(200);
+		}
+	}
+}
