604ba9c69377f364ed51f504171249a5d747019a
David Attias <attias666@gmail.com>
2017-03-02 17:16:22 +0100

    Fixes Caremen billing because of appconfiguration settings new fields

--- watchdog/src/main/java/io/spacedog/caremen/AppConfigurationSettings.java
+++ watchdog/src/main/java/io/spacedog/caremen/AppConfigurationSettings.java
@@ -1,7 +1,11 @@
 package io.spacedog.caremen;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import io.spacedog.utils.Settings;
 
+//ignore unknown fields
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AppConfigurationSettings extends Settings {
 	public double rateVAT;
 	public int scheduledCourseMinimumNoticeTimeInMinutes;
