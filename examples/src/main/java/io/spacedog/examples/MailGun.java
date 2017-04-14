11df243496825a1bcf2b20c12322a4647df6ad7b
David Attias <attias666@gmail.com>
2017-04-14 09:34:21 +0200

    Removes Unirest lib from examples sub project

--- examples/src/main/java/io/spacedog/examples/MailGun.java
+++ examples/src/main/java/io/spacedog/examples/MailGun.java
@@ -2,8 +2,8 @@ package io.spacedog.examples;
 
 import java.io.IOException;
 
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
+import io.spacedog.rest.SpaceRequest;
+import io.spacedog.rest.SpaceResponse;
 
 public class MailGun {
 
@@ -12,30 +12,35 @@ public class MailGun {
 			sendSandBoxedMail();
 		} catch (Throwable t) {
 			t.printStackTrace();
-			Unirest.shutdown();
 			System.exit(-1);
 		}
 	}
 
-	static void sendSandBoxedMail() throws UnirestException {
-		System.out.println(Unirest
-				.post("https://api.mailgun.net/v3/sandboxeb1564fe7eb04d7eab56e0c7e7684c65.mailgun.org/messages")//
+	static void sendSandBoxedMail() {
+		SpaceResponse response = SpaceRequest//
+				.post("/v3/sandboxeb1564fe7eb04d7eab56e0c7e7684c65.mailgun.org/messages")//
+				.backend("https://api.mailgun.net")//
 				.basicAuth("api", "key-04557379769f2de1d01fb12f7e837e1c")//
-				.field("from", "Mailgun Sandbox <postmaster@sandboxeb1564fe7eb04d7eab56e0c7e7684c65.mailgun.org>")//
-				.field("to", "David <david@spacedog.io>")//
-				.field("subject", "Hello David")//
-				.field("text",
+				.formField("from", "Mailgun Sandbox <postmaster@sandboxeb1564fe7eb04d7eab56e0c7e7684c65.mailgun.org>")//
+				.formField("to", "David <david@spacedog.io>")//
+				.formField("subject", "Hello David")//
+				.formField("text",
 						"Congratulations David, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free.")//
-				.asJson().getBody());
+				.go();
+
+		System.out.println(response.jsonNode());
 	}
 
-	static void sendDomainCheckedMail() throws UnirestException {
-		System.out.println(Unirest.post("https://api.mailgun.net/v3/api.spacedog.io/messages")//
+	static void sendDomainCheckedMail() {
+		SpaceResponse response = SpaceRequest.post("/v3/api.spacedog.io/messages")//
+				.backend("https://api.mailgun.net")//
 				.basicAuth("api", "key-04557379769f2de1d01fb12f7e837e1c")//
-				.field("from", "SpaceDog Platform <platform@spacedog.io>")//
-				.field("to", "David <attias666@gmail.com>")//
-				.field("subject", "My first gunned mail")//
-				.field("text", "We did it!!!")//
-				.asJson().getBody());
+				.formField("from", "SpaceDog Platform <platform@spacedog.io>")//
+				.formField("to", "David <attias666@gmail.com>")//
+				.formField("subject", "My first gunned mail")//
+				.formField("text", "We did it!!!")//
+				.go();
+
+		System.out.println(response.jsonNode());
 	}
 }
