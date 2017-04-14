3098a3714b34c3e389a26480d44945b4c5158fc4
David Attias <attias666@gmail.com>
2017-04-14 12:02:16 +0200

    Tests java sdk data endpoint 2

--- watchdog/src/main/java/io/spacedog/watchdog/DataResourceTestOften.java
+++ watchdog/src/main/java/io/spacedog/watchdog/DataResourceTestOften.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import io.spacedog.rest.SpaceRequest;
+import io.spacedog.rest.SpaceRequestException;
 import io.spacedog.rest.SpaceResponse;
 import io.spacedog.rest.SpaceTest;
 import io.spacedog.sdk.SpaceDog;
@@ -20,9 +21,9 @@ public class DataResourceTestOften extends SpaceTest {
 	public void createFindUpdateAndDelete() {
 
 		prepareTest();
-		SpaceDog test = resetTestBackend();
-		test.schema().set(SchemaResourceTestOften.buildCarSchema());
-		SpaceDog vince = signUp(test, "vince", "hi vince");
+		SpaceDog superadmin = resetTestBackend();
+		superadmin.schema().set(SchemaResourceTestOften.buildCarSchema());
+		SpaceDog vince = signUp(superadmin, "vince", "hi vince");
 
 		ObjectNode car = Json7.objectBuilder() //
 				.put("serialNumber", "1234567890") //
@@ -41,14 +42,12 @@ public class DataResourceTestOften extends SpaceTest {
 				.build();
 
 		// create
-
-		String id = SpaceRequest.post("/1/data/car").auth(vince).bodyJson(car).go(201)//
+		String id = vince.post("/1/data/car").bodyJson(car).go(201)//
 				.assertTrue("success").assertEquals("car", "type").assertNotNull("id")//
 				.getString("id");
 
 		// find by id
-
-		SpaceResponse res1 = SpaceRequest.get("/1/data/car/" + id).auth(vince).go(200)//
+		SpaceResponse res1 = vince.get("/1/data/car/" + id).go(200)//
 				.assertEquals("vince", "meta.createdBy")//
 				.assertEquals("vince", "meta.updatedBy")//
 				.assertDateIsRecent("meta.createdAt")//
@@ -59,14 +58,15 @@ public class DataResourceTestOften extends SpaceTest {
 
 		// find by full text search
 
-		SpaceRequest.get("/1/search/car").refresh().auth(vince).queryParam("q", "inVENT*").go(200)//
+		vince.get("/1/search/car").refresh().queryParam("q", "inVENT*").go(200)//
 				.assertEquals(id, "results.0.meta.id");
 
 		// update
 
-		SpaceRequest.put("/1/data/car/" + id).auth(vince).bodyJson("color", "blue").go(200);
+		vince.put("/1/data/car/" + id).bodyJson("color", "blue").go(200);
 
-		SpaceResponse res3 = SpaceRequest.get("/1/data/car/" + id).backend(test).go(200)//
+		SpaceResponse res3 = SpaceRequest.get("/1/data/car/" + id)//
+				.backend(superadmin).go(200)//
 				.assertEquals("vince", "meta.createdBy")//
 				.assertEquals("vince", "meta.updatedBy")//
 				.assertEquals(createdAt, "meta.createdAt")//
@@ -79,7 +79,63 @@ public class DataResourceTestOften extends SpaceTest {
 
 		// delete
 
-		SpaceRequest.delete("/1/data/car/" + id).auth(vince).go(200);
-		SpaceRequest.get("/1/data/car/" + id).auth(vince).go(404);
+		vince.delete("/1/data/car/" + id).go(200);
+		vince.get("/1/data/car/" + id).go(404);
+	}
+
+	@Test
+	public void createFindUpdateAndDeleteWithSdk() {
+
+		prepareTest();
+		SpaceDog superadmin = resetTestBackend();
+		superadmin.schema().set(SchemaResourceTestOften.buildCarSchema());
+		SpaceDog vince = signUp(superadmin, "vince", "hi vince");
+
+		ObjectNode car = Json7.objectBuilder() //
+				.put("serialNumber", "1234567890") //
+				.put("buyDate", "2015-01-09") //
+				.put("buyTime", "15:37:00") //
+				.put("buyTimestamp", "2015-01-09T15:37:00.123Z") //
+				.put("color", "red") //
+				.put("techChecked", false) //
+				.object("model") //
+				.put("description", "Cette voiture sent bon la France. Elle est inventive et raffinée.") //
+				.put("fiscalPower", 8) //
+				.put("size", 4.67) //
+				.end().object("location") //
+				.put("lat", -55.6765) //
+				.put("lon", -54.6765) //
+				.build();
+
+		// create
+		String id = vince.data2().create("car", car);
+
+		// find by id
+		ObjectNode carbis = vince.data2().get("car", id);
+		assertEquals("vince", Json7.checkString(carbis, "meta.createdBy").get());
+		assertEquals("vince", Json7.checkString(carbis, "meta.updatedBy").get());
+		assertNotNull(Json7.checkString(carbis, "meta.createdAt"));
+		carbis.remove("meta");
+		assertEquals(car, carbis);
+
+		// find by full text search
+
+		SpaceRequest.get("/1/search/car").refresh().auth(vince).queryParam("q", "inVENT*").go(200)//
+				.assertEquals(id, "results.0.meta.id");
+
+		// update
+
+		vince.data2().save("car", id, Json7.object("color", "blue"), false);
+		carbis = vince.data2().get("car", id);
+		assertEquals("blue", carbis.get("color").asText());
+
+		// delete
+
+		vince.data2().delete("car", id);
+		try {
+			vince.data2().delete("car", id);
+		} catch (SpaceRequestException e) {
+			assertEquals(404, e.httpStatus());
+		}
 	}
 }
