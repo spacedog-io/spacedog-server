c52c1f108c331e8364f18e12f6f1c10327476238
David Attias <attias666@gmail.com>
2017-04-19 17:43:54 +0200

    Refactors and fixes SendPulseResourceTest

--- test/src/main/java/io/spacedog/test/sendpulse/SendPulseResourceTest.java
+++ test/src/main/java/io/spacedog/test/sendpulse/SendPulseResourceTest.java
@@ -8,67 +8,83 @@ import java.util.Collections;
 import org.joda.time.DateTime;
 
 import io.spacedog.model.SendPulseSettings;
-import io.spacedog.rest.SpaceRequest;
-import io.spacedog.rest.SpaceTarget;
+import io.spacedog.rest.SpaceEnv;
+import io.spacedog.rest.SpaceRequestException;
 import io.spacedog.rest.SpaceTest;
 import io.spacedog.sdk.SpaceDog;
+import io.spacedog.utils.Utils;
 
 public class SendPulseResourceTest extends SpaceTest {
 
+	// @Test
 	public void testSendPulseResource() {
+
+		// prepare
+		prepareTest();
 
 		// this must come before preparing the test
 		// or else the test flag is set
 		// and the sendpulse backend files are not reset
 		initSendPulseTestWebApp();
 
-		// prepare
-		prepareTest();
-		SpaceRequest.env().target(SpaceTarget.local);
-		SpaceDog test = resetTestBackend();
+		// reset test backend
+		SpaceDog superadmin = resetTestBackend();
 		SpaceDog fred = signUp("test", "fred", "hi fred");
+		SpaceDog vince = signUp("test", "vince", "hi vince");
+		superadmin.credentials().setRole(fred.id(), "pusher");
 
 		// set sendpulse test settings
 		SendPulseSettings settings = new SendPulseSettings();
-		settings.clientId = SpaceRequest.env().get("spacedog.test.sendpulse.client.id");
-		settings.clientSecret = SpaceRequest.env().get("spacedog.test.sendpulse.client.secret");
-		settings.authorizedRoles = Collections.singleton("user");
-		test.settings().save(settings);
+		settings.clientId = SpaceEnv.defaultEnv()//
+				.get("spacedog.test.sendpulse.client.id");
+		settings.clientSecret = SpaceEnv.defaultEnv()//
+				.get("spacedog.test.sendpulse.client.secret");
+		settings.authorizedRoles = Collections.singleton("pusher");
+		superadmin.settings().save(settings);
 
 		// fred can get all websites
-		SpaceRequest.get("/1/sendpulse/push/websites").auth(fred).go(200);
+		fred.get("/1/sendpulse/push/websites").go(200);
 
-		// fred can create a web push to sendpulse.www.spacedog.io
-		SpaceRequest.post("/1/sendpulse/push/tasks").auth(fred)//
+		// fred fails to web push to sendpulse.www.spacedog.io
+		// since no website_id parameter
+		fred.post("/1/sendpulse/push/tasks")//
 				.formField("title", "Hello")//
 				.formField("body", DateTime.now().toString())//
 				.go(400)//
 				.assertEquals("sendpulse-1255", "error.code");
 
-		// fred can create a web push to sendpulse.www.spacedog.io
-		SpaceRequest.post("/1/sendpulse/push/tasks").auth(fred)//
+		// fred web pushes to sendpulse.www.spacedog.io
+		fred.post("/1/sendpulse/push/tasks")//
 				.formField("title", "Hello")//
 				.formField("body", DateTime.now().toString())//
 				.formField("website_id", "22573")//
 				.formField("ttl", "300")// 5 minutes
 				.go(200);
 
-		// settings forbid admins to get all websites nor create push tasks
-		SpaceRequest.get("/1/sendpulse/push/websites").auth(test).go(403);
-		SpaceRequest.post("/1/sendpulse/push/tasks").auth(test).go(403);
+		// vince fails to get all websites nor push tasks
+		// since he hasn't got the 'pusher' role
+		vince.get("/1/sendpulse/push/websites").go(403);
+		vince.post("/1/sendpulse/push/tasks").go(403);
 	}
 
 	private void initSendPulseTestWebApp() {
 
 		// prepare
-		SpaceRequest.env().target(SpaceTarget.production);
+		String password = SpaceEnv.defaultEnv()//
+				.get("spacedog.test.sendpulse.backend.password");
+		SpaceDog sendpulse = SpaceDog.backend("https://sendpulse.spacedog.io")//
+				.username("sendpulse");
 
-		// if sendpulde test web app already initialized then return
-		if (SpaceRequest.get("/1/file/www/index.html").backend("sendpulse")//
-				.go(200, 404).status() == 200)
-			return;
-
-		SpaceDog sendpulse = resetBackend("sendpulse", "sendpulse", "hi sendpulse");
+		try {
+			sendpulse.login(password);
+		} catch (SpaceRequestException e) {
+			if (e.httpStatus() == 401) {
+				sendpulse.admin().createBackend(//
+						"sendpulse", password, "platform@spacedog.io", false);
+				sendpulse.login(password);
+			} else
+				throw e;
+		}
 
 		// upload web site
 		upload(sendpulse, "index.html");
@@ -76,9 +92,7 @@ public class SendPulseResourceTest extends SpaceTest {
 		upload(sendpulse, "sp-push-worker.js");
 	}
 
-	private void upload(SpaceDog backend, String name) {
-		SpaceRequest.put("/1/file/www/" + name)//
-		.bodyResource(this.getClass(), name).auth(backend)//
-				.go(200);
+	private void upload(SpaceDog dog, String name) {
+		dog.file().save("/www/" + name, Utils.readResource(this.getClass(), name));
 	}
 }
