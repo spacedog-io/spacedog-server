82505184383aaf13db2bae46542df963d2d865e3
David Attias <davattias@users.noreply.github.com>
2017-10-21 10:49:19 +0200

    Refactors share service interface

--- server/src/main/java/io/spacedog/server/ShareResource.java
+++ server/src/main/java/io/spacedog/server/ShareResource.java
@@ -5,6 +5,8 @@ package io.spacedog.server;
 
 import java.util.UUID;
 
+import com.google.common.base.Strings;
+
 import io.spacedog.model.DataPermission;
 import io.spacedog.model.ShareSettings;
 import io.spacedog.utils.Credentials;
@@ -15,10 +17,9 @@ import net.codestory.http.annotations.Delete;
 import net.codestory.http.annotations.Get;
 import net.codestory.http.annotations.Post;
 import net.codestory.http.annotations.Prefix;
-import net.codestory.http.annotations.Put;
 import net.codestory.http.payload.Payload;
 
-@Prefix("/1/share")
+@Prefix("/1/shares")
 public class ShareResource extends S3Resource {
 
 	private static final String SHARE_BUCKET_SUFFIX = "shared";
@@ -34,28 +35,28 @@ public class ShareResource extends S3Resource {
 		return doList(SHARE_BUCKET_SUFFIX, WebPath.ROOT, context);
 	}
 
-	@Get("/:uuid/:fileName")
-	@Get("/:uuid/:fileName/")
-	public Payload get(String uuid, String fileName, Context context) {
+	@Get("/:id")
+	@Get("/:id/")
+	public Payload get(String id, Context context) {
 
 		boolean checkOwnership = checkPermissionAndIsOwnershipRequired(//
 				DataPermission.read, DataPermission.read_all);
 
-		return doGet(SHARE_BUCKET_SUFFIX, WebPath.newPath(uuid, fileName), //
+		return doGet(SHARE_BUCKET_SUFFIX, WebPath.newPath(id), //
 				context, checkOwnership);
 	}
 
-	@Post("/:fileName")
-	@Post("/:fileName/")
-	// deprecated since not idempotent
-	@Put("/:fileName")
-	@Put("/:fileName/")
-	public Payload post(String fileName, byte[] bytes, Context context) {
+	@Post("")
+	@Post("/")
+	public Payload post(byte[] bytes, Context context) {
 		Credentials credentials = checkPermission(DataPermission.create);
+		String fileName = context.get("fileName");
 		ShareSettings settings = shareSettings();
-		String uuid = UUID.randomUUID().toString();
+		String id = UUID.randomUUID().toString();
+		if (!Strings.isNullOrEmpty(fileName))
+			id = id + '-' + fileName;
 		return doUpload(SHARE_BUCKET_SUFFIX, "/1/share", credentials, //
-				WebPath.newPath(uuid, fileName), bytes, context, settings.enableS3Location);
+				WebPath.newPath(id), bytes, context, settings.enableS3Location);
 	}
 
 	@Delete("")
@@ -65,13 +66,13 @@ public class ShareResource extends S3Resource {
 		return doDelete(SHARE_BUCKET_SUFFIX, WebPath.ROOT, false, false);
 	}
 
-	@Delete("/:uuid/:fileName")
-	@Delete("/:uuid/:fileName/")
-	public Payload delete(String uuid, String fileName, Context context) {
+	@Delete("/:id")
+	@Delete("/:id/")
+	public Payload delete(String id, Context context) {
 		boolean checkOwnership = checkPermissionAndIsOwnershipRequired(//
 				DataPermission.delete, DataPermission.delete_all);
 
-		return doDelete(SHARE_BUCKET_SUFFIX, WebPath.newPath(uuid, fileName), //
+		return doDelete(SHARE_BUCKET_SUFFIX, WebPath.newPath(id), //
 				true, checkOwnership);
 	}
 
