fea1a9714fd1fb7fb68b643a280d53de993e53f0
David Attias <davattias@users.noreply.github.com>
2017-11-15 18:39:00 +0100

    Merges all service error filter tests in one class file

--- test/src/main/java/io/spacedog/test/ServiceErrorFilterTest.java
+++ test/src/main/java/io/spacedog/test/ServiceErrorFilterTest.java
@@ -2,9 +2,29 @@ package io.spacedog.test;
 
 import org.junit.Test;
 
+import io.spacedog.client.SpaceDog;
 import io.spacedog.http.SpaceRequest;
 
 public class ServiceErrorFilterTest extends SpaceTest {
+
+	@Test
+	public void catchesFluentResourceErrors() {
+
+		prepareTest();
+		SpaceDog test = resetTestBackend();
+
+		// should fail to access invalid route
+
+		SpaceRequest.get("/1/toto").backend(test).go(404)//
+				.assertFalse("success")//
+				.assertEquals("path [/1/toto] invalid", "error.message");
+
+		// should fail to use this method for this valid route
+
+		SpaceRequest.put("/1/login").auth(test).go(405)//
+				.assertFalse("success")//
+				.assertEquals("method [PUT] invalid for path [/1/login]", "error.message");
+	}
 
 	@Test
 	public void notifySuperdogsForInternalServerErrors() {
