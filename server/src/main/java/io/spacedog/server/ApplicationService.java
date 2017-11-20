29e184146a42a3f36f1738de5b63bfef8279d696
David Attias <davattias@users.noreply.github.com>
2017-11-20 15:21:52 +0100

    Improves /applications: adds GET /applications and fixes PUT /applications/:name/:service to update app when it already exists

--- server/src/main/java/io/spacedog/server/ApplicationService.java
+++ server/src/main/java/io/spacedog/server/ApplicationService.java
@@ -3,16 +3,23 @@
  */
 package io.spacedog.server;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-
-import org.elasticsearch.common.Strings;
+import java.util.stream.Collectors;
 
 import com.amazonaws.services.sns.model.CreatePlatformApplicationRequest;
 import com.amazonaws.services.sns.model.DeletePlatformApplicationRequest;
+import com.amazonaws.services.sns.model.ListPlatformApplicationsResult;
 import com.amazonaws.services.sns.model.PlatformApplication;
+import com.amazonaws.services.sns.model.SetPlatformApplicationAttributesRequest;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.google.common.base.Strings;
 
 import io.spacedog.model.PushService;
+import io.spacedog.utils.Exceptions;
 import io.spacedog.utils.Json;
+import io.spacedog.utils.Utils;
 import net.codestory.http.annotations.Delete;
 import net.codestory.http.annotations.Get;
 import net.codestory.http.annotations.Put;
@@ -26,14 +33,20 @@ public class ApplicationService extends SpaceService {
 
 	@Get("/1/applications")
 	@Get("/1/applications/")
-	public Payload getApplications() {
-		SpaceContext.credentials().checkAtLeastAdmin();
-		return JsonPayload.ok().build();
-	}
+	public Object getApplications() {
 
-	private static class ApplicationPushCredentials {
-		public String principal;
-		public String credentials;
+		SpaceContext.credentials().checkAtLeastAdmin();
+		String backendId = SpaceContext.backendId();
+
+		ListPlatformApplicationsResult applications = AwsSnsPusher.getSnsClient()//
+				.listPlatformApplications();
+
+		List<PushApplication> pushApps = applications.getPlatformApplications().stream()//
+				.map(application -> toPushApplication(application))//
+				.filter(application -> backendId.equals(application.backendId))//
+				.collect(Collectors.toList());
+
+		return Json.toString(pushApps);
 	}
 
 	@Put("/1/applications/:name/:pushService")
@@ -42,19 +55,40 @@ public class ApplicationService extends SpaceService {
 
 		SpaceContext.credentials().checkAtLeastAdmin();
 
-		ApplicationPushCredentials credentials = Json.toPojo(//
-				body, ApplicationPushCredentials.class);
+		PushApplication pushApp = new PushApplication();
+		pushApp.backendId = SpaceContext.backendId();
+		pushApp.name = name;
+		pushApp.service = PushService.valueOf(pushService);
+		pushApp.credentials = Json.toPojo(body, PushApplication.Credentials.class);
 
-		CreatePlatformApplicationRequest request = new CreatePlatformApplicationRequest()//
-				.withName(toApplicationId(name))//
-				.withPlatform(pushService);
+		Optional<PlatformApplication> application = getPlatformApplication(pushApp);
 
-		request.addAttributesEntry("PlatformCredential", credentials.credentials);
+		if (application.isPresent()) {
+			SetPlatformApplicationAttributesRequest request = new SetPlatformApplicationAttributesRequest()//
+					.withPlatformApplicationArn(application.get().getPlatformApplicationArn());
 
-		if (!Strings.isNullOrEmpty(credentials.principal))
-			request.addAttributesEntry("PlatformPrincipal", credentials.principal);
+			if (!Strings.isNullOrEmpty(pushApp.credentials.credentials))
+				request.addAttributesEntry("PlatformCredential", pushApp.credentials.credentials);
 
-		AwsSnsPusher.getSnsClient().createPlatformApplication(request);
+			if (!Strings.isNullOrEmpty(pushApp.credentials.principal))
+				request.addAttributesEntry("PlatformPrincipal", pushApp.credentials.principal);
+
+			AwsSnsPusher.getSnsClient().setPlatformApplicationAttributes(request);
+
+		} else {
+
+			CreatePlatformApplicationRequest request = new CreatePlatformApplicationRequest()//
+					.withName(pushApp.applicationId())//
+					.withPlatform(pushService);
+
+			if (!Strings.isNullOrEmpty(pushApp.credentials.credentials))
+				request.addAttributesEntry("PlatformCredential", pushApp.credentials.credentials);
+
+			if (!Strings.isNullOrEmpty(pushApp.credentials.principal))
+				request.addAttributesEntry("PlatformPrincipal", pushApp.credentials.principal);
+
+			AwsSnsPusher.getSnsClient().createPlatformApplication(request);
+		}
 
 		return JsonPayload.ok().build();
 	}
@@ -63,10 +97,14 @@ public class ApplicationService extends SpaceService {
 	@Delete("/1/applications/:name/:pushService/")
 	public Payload deleteApplication(String name, String pushService) {
 
-		SpaceContext.credentials().checkAtLeastAdmin();
+		SpaceContext.credentials().checkAtLeastSuperAdmin();
 
-		Optional<PlatformApplication> application = AwsSnsPusher.getApplication(//
-				toApplicationId(name), PushService.valueOf(pushService));
+		PushApplication pushApp = new PushApplication();
+		pushApp.backendId = SpaceContext.backendId();
+		pushApp.name = name;
+		pushApp.service = PushService.valueOf(pushService);
+
+		Optional<PlatformApplication> application = getPlatformApplication(pushApp);
 
 		if (application.isPresent()) {
 			String applicationArn = application.get().getPlatformApplicationArn();
@@ -83,8 +121,49 @@ public class ApplicationService extends SpaceService {
 	// Implementation
 	//
 
-	private String toApplicationId(String name) {
-		return SpaceContext.backendId() + '-' + name;
+	private Optional<PlatformApplication> getPlatformApplication(PushApplication pushApp) {
+		return AwsSnsPusher.getApplication(pushApp.applicationId(), pushApp.service);
+	}
+
+	private PushApplication fromApplicationArn(String arn) {
+		PushApplication pushApp = new PushApplication();
+		String[] splitedArn = Utils.splitBySlash(arn);
+		if (splitedArn.length != 3)
+			throw Exceptions.runtime("invalid amazon platform application arn [%s]", arn);
+		pushApp.service = PushService.valueOf(splitedArn[1]);
+		String[] splitedAppId = Utils.splitByDash(splitedArn[2]);
+		if (splitedAppId.length == 2) {
+			pushApp.backendId = splitedAppId[0];
+			pushApp.name = splitedAppId[1];
+		} else
+			pushApp.name = splitedAppId[0];
+		return pushApp;
+	}
+
+	private PushApplication toPushApplication(PlatformApplication application) {
+		PushApplication pushApp = fromApplicationArn(application.getPlatformApplicationArn());
+		pushApp.attributes = application.getAttributes();
+		return pushApp;
+	}
+
+	@SuppressWarnings("unused")
+	public static class PushApplication {
+		public String name;
+		public String backendId;
+		public PushService service;
+		public Map<String, String> attributes;
+		@JsonIgnore
+		public Credentials credentials;
+
+		public static class Credentials {
+			public String principal;
+			public String credentials;
+		}
+
+		public String applicationId() {
+			return backendId + '-' + name;
+		}
+
 	}
 
 	//
