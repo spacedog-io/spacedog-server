90dccee26bec46800026a152d64b4a6e6453bdef
davattias <david@spacedog.io>
2017-12-01 16:18:10 +0100

    Removes rm-snapshot-files.sh script since already exists in prod folder

--- watchdog/rm-snapshot-files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-repos=( 2017-12 2017-13 2017-14 2017-15 2017-16 2017-17 2017-18 2017-19 )
-
-for repo in "${repos[@]}"
-do
-	aws s3 rm --recursive s3://spacedog-snapshots/"$repo"
-done
