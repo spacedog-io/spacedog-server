240c84d10122c2e3b1572add24579273ef924875
davattias <david@spacedog.io>
2017-12-06 18:21:17 +0100

    Adds ClassResources.loadToBytes()

--- sdk/src/main/java/io/spacedog/utils/ClassResources.java
+++ sdk/src/main/java/io/spacedog/utils/ClassResources.java
@@ -1,22 +1,35 @@
 package io.spacedog.utils;
 
+import java.net.URL;
+
 import com.google.common.io.Resources;
 
 public class ClassResources {
 
 	public static String loadToString(Object context, String resourceName) {
 		try {
-			Class<?> contextClass = context instanceof Class<?> //
-					? (Class<?>) context
-					: context.getClass();
-
 			return Resources.toString(//
-					Resources.getResource(contextClass, resourceName), //
-					Utils.UTF8);
+					toUrl(context, resourceName), Utils.UTF8);
 
 		} catch (Exception e) {
 			throw Exceptions.runtime(e, "error loading resource [%s]", resourceName);
 		}
 	}
 
+	public static byte[] loadToBytes(Object context, String resourceName) {
+		try {
+			return Resources.toByteArray(toUrl(context, resourceName));
+
+		} catch (Exception e) {
+			throw Exceptions.runtime(e, "error loading resource [%s]", resourceName);
+		}
+	}
+
+	public static URL toUrl(Object context, String resourceName) {
+		Class<?> contextClass = context instanceof Class<?> //
+				? (Class<?>) context
+				: context.getClass();
+
+		return Resources.getResource(contextClass, resourceName);
+	}
 }
