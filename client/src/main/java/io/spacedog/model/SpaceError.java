68f097a33a12298254fcebf248862d3de21ce27e
davattias <david@spacedog.io>
2017-12-29 10:30:18 +0100

    Add SpaceError model class

---
+++ client/src/main/java/io/spacedog/model/SpaceError.java
@@ -0,0 +1,20 @@
+package io.spacedog.model;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonAutoDetect(fieldVisibility = Visibility.ANY, //
+		getterVisibility = Visibility.NONE, //
+		isGetterVisibility = Visibility.NONE, //
+		setterVisibility = Visibility.NONE)
+public class SpaceError {
+	public String code;
+	public String type;
+	public String message;
+	public String[] trace;
+	public ObjectNode details;
+	public SpaceError cause;
+}
