8acf21124b77c11388bb158b363ca36e8176ddf4
davattias <david@spacedog.io>
2018-04-15 22:32:16 +0200

    Add converters from DateTime to Epoch double dates

--- client/src/main/java/io/spacedog/utils/DateTimeZones.java
+++ client/src/main/java/io/spacedog/utils/DateTimeZones.java
@@ -1,7 +1,17 @@
 package io.spacedog.utils;
 
+import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 
 public class DateTimeZones {
 	public final static DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+	public static double toEpochSeconds(DateTime date) {
+		return ((double) date.getMillis()) / 1000;
+	}
+
+	public static DateTime toDateTime(double epochSeconds) {
+		return new DateTime((long) (epochSeconds * 1000));
+	}
+
 }
