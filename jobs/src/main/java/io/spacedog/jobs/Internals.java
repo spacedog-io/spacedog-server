a3a85f86c3a0f019ea26d5786b207d02f3ac307d
davattias <david@spacedog.io>
2018-06-13 17:51:07 +0200

    Add Internals.notify(topicId, title, throwable)

--- jobs/src/main/java/io/spacedog/jobs/Internals.java
+++ jobs/src/main/java/io/spacedog/jobs/Internals.java
@@ -4,12 +4,17 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.amazonaws.services.sns.model.PublishRequest;
+import com.google.common.base.Throwables;
 
 import io.spacedog.utils.Utils;
 
 public class Internals {
 
 	private final AmazonSNS sns;
+
+	public void notify(String topicId, String title, Throwable t) {
+		notify(toString(), title, Throwables.getStackTraceAsString(t));
+	}
 
 	public void notify(String topicId, String title, String message) {
 
