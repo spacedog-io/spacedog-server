ead9487f26ac5378be00d39e3fd1c586ec0fac18
davattias <david@spacedog.io>
2018-10-11 17:15:30 +0200

    Fix linkedin resty test: linkedin now returns 401 if invalid code

--- test/src/main/java/io/spacedog/test/credentials/LinkedinRestyTest.java
+++ test/src/main/java/io/spacedog/test/credentials/LinkedinRestyTest.java
@@ -49,14 +49,14 @@ public class LinkedinRestyTest extends SpaceTest {
 				.backend(superadmin.backend())//
 				.formField("code", "XXX")//
 				.formField("redirect_uri", "XXX")//
-				.go(400);
+				.go(401);
 
 		// fails to create linkedin credentials if invalid code
 		SpaceRequest.post("/1/login/linkedin")//
 				.backend(superadmin.backend())//
 				.formField("code", "XXX")//
 				.formField("redirect_uri", redirectUri)//
-				.go(400);
+				.go(401);
 
 		// fails to create linkedin credentials if invalid code
 		// if no redirect_uri parameter is set spacedog uses
@@ -64,6 +64,6 @@ public class LinkedinRestyTest extends SpaceTest {
 		SpaceRequest.post("/1/login/linkedin")//
 				.backend(superadmin.backend())//
 				.formField("code", "XXX")//
-				.go(400);
+				.go(401);
 	}
 }
