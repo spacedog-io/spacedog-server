f8cb58b2e3aede64d1717ebc5b2fdc42a86040b0
davattias <david@spacedog.io>
2018-11-26 19:41:43 +0100

    Add test on new french prefix analyzer

--- test/src/main/java/io/spacedog/test/data/SearchRestyFrenchTest.java
+++ test/src/main/java/io/spacedog/test/data/SearchRestyFrenchTest.java
@@ -10,11 +10,61 @@ import io.spacedog.client.elastic.ESQueryBuilders;
 import io.spacedog.client.elastic.ESSearchSourceBuilder;
 import io.spacedog.client.schema.Schema;
 import io.spacedog.test.SpaceTest;
+import io.spacedog.utils.ClassResources;
 import io.spacedog.utils.Json;
 
 public class SearchRestyFrenchTest extends SpaceTest {
 
 	private SpaceDog superadmin;
+
+	@Test
+	public void prefixSearchWithFrenchSpecificAnalyzer() {
+
+		prepareTest(true, true);
+		superadmin = clearServer();
+		byte[] mapping = ClassResources.loadAsBytes(this, "message.mapping.json");
+		superadmin.schemas().set(Json.toPojo(mapping, Schema.class));
+
+		index("Google");
+
+		matchPrefix("g");
+		matchPrefix("G");
+		matchPrefix("go");
+		matchPrefix("Go");
+		matchPrefix("goo");
+		matchPrefix("GOO");
+		matchPrefix("goog");
+		matchPrefix("googl");
+		matchPrefix("google");
+		matchPrefix("goOgLe");
+		noMatchPrefix("googles");
+
+		index("L'étable de Marie");
+
+		matchPrefix("l");
+		matchPrefix("l'");
+		matchPrefix("l'ét");
+		matchPrefix("l'etAb");
+		matchPrefix("etab");
+		matchPrefix("d");
+		matchPrefix("de");
+		matchPrefix("maR");
+
+		noMatchPrefix("le");
+		noMatchPrefix("létable");
+		noMatchPrefix("tabler");
+		noMatchPrefix("marier");
+		noMatchPrefix("maa");
+		noMatchPrefix("tabe");
+
+		index("j' zi");
+
+		matchPrefix("j");
+		matchPrefix("j'z");
+		matchPrefix("j z");
+
+		noMatchPrefix("jz");
+	}
 
 	@Test
 	public void searchWithFrenchMaxAnalyser() {
@@ -84,17 +134,29 @@ public class SearchRestyFrenchTest extends SpaceTest {
 
 	private void match(String text) {
 		assertEquals(1, search(text, "text"));
-		// assertEquals(1, search(text, "_all"));
+	}
+
+	private void matchPrefix(String text) {
+		assertEquals(1, searchPrefix(text, "text"));
 	}
 
 	private void noMatch(String text) {
 		assertEquals(0, search(text, "text"));
-		// assertEquals(0, search(text, "_all"));
+	}
+
+	private void noMatchPrefix(String text) {
+		assertEquals(0, searchPrefix(text, "text"));
 	}
 
 	private long search(String text, String field) {
 		ESSearchSourceBuilder source = ESSearchSourceBuilder.searchSource()//
 				.query(ESQueryBuilders.matchQuery(field, text));
+		return superadmin.data().prepareSearch().refresh(true).source(source.toString()).go().total;
+	}
+
+	private long searchPrefix(String text, String field) {
+		ESSearchSourceBuilder source = ESSearchSourceBuilder.searchSource()//
+				.query(ESQueryBuilders.matchPhrasePrefixQuery(field, text));
 		return superadmin.data().prepareSearch().refresh(true).source(source.toString()).go().total;
 	}
 }
